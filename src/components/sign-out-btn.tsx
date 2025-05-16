aa183cee9e30c6894a0ac0650e70c3b300527da8
Amir Siami <siami.amir@gmail.com>
2025-05-16 12:54:46 +0330

    some component added

---
+++ src/components/sign-out-btn.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+// import { logOut } from "@/actions/actions";
+import { Button } from "./ui/button";
+import { useTransition } from "react";
+
+export default function SignOutBtn() {
+    const [isPending, startTransition] = useTransition();
+
+    return (
+        <Button
+            disabled={isPending}
+        >
+            Sign out
+        </Button>
+    );
+}
